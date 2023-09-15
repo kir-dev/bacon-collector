933b637c6d6743666a527a50cd7c1691d60e33f4
Horváth István <horvatska01@gmail.com>
2023-09-15 21:14:22 +0200

    chore: add pigeon maps

--- src/components/map-content.tsx
+++ src/components/map-content.tsx
@@ -1,6 +1,15 @@
 import { useLocationItems } from '@/hooks/useLocationItems'
+import { Map } from 'pigeon-maps'
+import { osm } from 'pigeon-maps/providers'
+import React from 'react'
 
 export function MapContent() {
   const locations = useLocationItems()
-  return <div>{JSON.stringify(locations.data)}</div>
+  return <div className='h-screen'>
+    <div>{JSON.stringify(locations.data)}</div>
+    <Map
+      provider={osm}
+      defaultCenter={[47.4979, 19.0402]}
+      defaultZoom={11} />
+  </div>
 }
--- src/components/map-marker.tsx
+++ src/components/map-marker.tsx
@@ -1,14 +1,12 @@
-import { Coords } from 'google-map-react'
-
-interface Props extends Coords {
-  text: string
+interface Props {
+  text: string;
 }
 
 export function MapMarker({ text }: Props) {
   return (
-    <div className="-translate-x-1/2 flex items-center flex-col gap-2">
-      <div className="w-5 h-5 bg-green-500 rounded-full border-2 border-white" />
-      <div className="text-xs text-center px-3 py-1 rounded-full bg-gray-900 text-white w-fit">
+    <div className='-translate-x-1/2 flex items-center flex-col gap-2'>
+      <div className='w-5 h-5 bg-green-500 rounded-full border-2 border-white' />
+      <div className='text-xs text-center px-3 py-1 rounded-full bg-gray-900 text-white w-fit'>
         <p>{text}</p>
       </div>
     </div>
