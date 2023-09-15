b96fe592a5f2351c31429b899e51302ab85dcf33
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-15 19:11:27 +0200

    feat: added map marker

---
+++ src/components/map-marker.tsx
@@ -0,0 +1,16 @@
+import { Coords } from "google-map-react";
+
+interface Props extends Coords {
+  text: string;
+}
+
+export function MapMarker({ text }: Props) {
+  return (
+    <div className="-translate-x-1/2 flex items-center flex-col gap-2">
+      <div className="w-5 h-5 bg-green-500 rounded-full border-2 border-white" />
+      <div className="text-xs text-center px-3 py-1 rounded-full bg-gray-900 text-white w-fit">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+}
